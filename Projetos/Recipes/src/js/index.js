4a1e16865b0031be3830bede65a7e52cafcfb09c
Bruno Cerqueira <brunocerqueiram@gmail.com>
2018-07-19 01:31:56 -0300

    MVC in JavaScript created, Search event completed.

--- Projetos/Recipes/src/js/index.js
+++ Projetos/Recipes/src/js/index.js
@@ -1,22 +1,29 @@
-/*import str from './models/Search';
-import { add, multiply, op } from './views/searchView';
+import Search from './models/Search';
 import * as searchView from './views/searchView';
-console.log(`Using imported functions ${add(op, 2)} and ${multiply(3, 5)}. ${str}`);
-console.log(searchView.op);
-*/
-//http://food2fork.com/api/search
-//034df1eb03833db0692e7c3d0e82f00f
-import axios from 'axios';
+import { elements } from './views/base';
+const state = {};
 
-async function getResult(query){
-  const proxy = 'https://cors-anywhere.herokuapp.com/'
-  const key = '034df1eb03833db0692e7c3d0e82f00f'
-  try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res.data.recipes);
-  } catch(error) {
-    alert(error);
+const controlSearch = async () => {
+  // Get query from view
+  const query = searchView.getInput();
+
+  if(query) {
+    // New search object and add to state
+    state.search = new Search(query);
+
+    // Prepare view for result
+    searchView.clearInput();
+    searchView.clearResults();
+
+    // Search for recipes
+    await state.search.getResults();
+
+    // render result on UI
+    searchView.renderResults(state.search.results);
   }
 }
 
-getResult('pizza');
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
--- Projetos/Recipes/src/js/models/Search.js
+++ Projetos/Recipes/src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults(){
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = '034df1eb03833db0692e7c3d0e82f00f'
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.results = res.data.recipes;
+      // console.log(this.result);
+    } catch(error) {
+      alert(error);
+    }
+  }
+}
+
+
