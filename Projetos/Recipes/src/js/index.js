83c3409030db6ff8fec283e73249cf6fca03ab8b
Bruno Cerqueira <brunocerqueiram@gmail.com>
2018-08-03 19:58:53 -0300

    List controller, view events implemented.

--- Projetos/Recipes/src/js/index.js
+++ Projetos/Recipes/src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 
@@ -97,6 +98,39 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/**
+ * List Controller
+ */
+
+const controlList = () => {
+  // Create a new lis IF there is none yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and user interface
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // Handle delete button
+  if(e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // Delete from state
+    state.list.deleteItem(id);
+    // Delete from UI
+    listView.deleteItem(id);
+
+  // Handle count update
+  } else if(e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -109,6 +143,8 @@ elements.recipe.addEventListener('click', e => {
     // Incresase
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+    controlList();
   }
 });
 
